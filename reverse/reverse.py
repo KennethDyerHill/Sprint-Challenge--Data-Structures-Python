20a5fe33426f89cf1a29ca6b3d225a9f7cdfc070
KennethDyerHill <52476371+KennethDyerHill@users.noreply.github.com>
2020-09-01 18:08:17 -0400

    Push

--- reverse/reverse.py
+++ reverse/reverse.py
@@ -1,6 +1,8 @@
 class Node:
     def __init__(self, value=None, next_node=None):
+
         self.value = value
+    
         self.next_node = next_node
 
     def get_value(self):
@@ -10,33 +12,53 @@ class Node:
         return self.next_node
 
     def set_next(self, new_next):
+   
         self.next_node = new_next
 
 class LinkedList:
-    def __init__(self):
-        self.head = None
+  def __init__(self):
+    
+    self.head = None
 
-    def add_to_head(self, value):
-        node = Node(value)
+  def add_to_head(self, value):
+    node = Node(value)
+    if self.head is not None:
+      node.set_next(self.head)
+    
+    self.head = node
 
-        if self.head is not None:
-            node.set_next(self.head)
+  def contains(self, value):
+    if not self.head:
+      return False
+    # get a reference to the node we're currently at; update this as we traverse the list
+    current = self.head
+    # check to see if we're at a valid node 
+    while current:
+      # return True if the current value we're looking at matches our target value
+      if current.get_value() == value:
+        return True
+      
+      current = current.get_next()
+    
+    return False
 
-        self.head = node
+  def reverse_list(self):
+    
 
-    def contains(self, value):
-        if not self.head:
-            return False
+    current_value = self.head
+    next_value = None
+    previous_value = None
 
-        current = self.head
+    
+    if current_value is None:
+      return None
 
-        while current:
-            if current.get_value() == value:
-                return True
-
-            current = current.get_next()
-
-        return False
-
-    def reverse_list(self, node, prev):
-        pass
+    
+    else:
+      while current_value is not None:
+        
+        next_value = current_value.next_node
+        current_value.next_node = previous_value
+        previous_value = current_value
+        current_value = next_value
+        self.head = previous_value
--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,43 @@
+from doubly_linked_list import DoublyLinkedList
+
+
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.current = None
+        self.storage = DoublyLinkedList()
 
     def append(self, item):
-        pass
+        
+        if self.capacity > self.storage.length:
+            
+            self.storage.add_to_head(item)
+            
+            self.current = self.storage.tail
+        else:
+            
+            self.current.value = item
+
+            
+            if self.current is self.storage.head:
+                
+                self.current = self.storage.tail
+            else:
+                
+                self.current = self.current.prev
 
     def get(self):
-        pass
+        
+        list_buffer_contents = []
+
+        
+        last_node_item = self.storage.tail
+
+        
+        while last_node_item is not None:
+
+            
+            list_buffer_contents.append(last_node_item.value)
+            last_node_item = last_node_item.prev
+
+        return list_buffer_contents
